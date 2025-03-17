158af17ebe170694b6ed9eb404a22fd9a2dc8c30
chris-mathevet <chris.mathevet@etu.univ-orleans.fr>
2025-03-17 17:07:48 +0100

    début connexion BD

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:csv/csv.dart';
+import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Future<List<List<dynamic>>> loadCSV() async {
+    final String rawData = await rootBundle.loadString('assets/pass.csv');
+    List<List<dynamic>> csvTable = const CsvToListConverter().convert(rawData);
+
+    return csvTable;
+  }
+
+  List<List<dynamic>> passData = await loadCSV();
+
+  await Supabase.initialize(
+    url: passData[0][0],    // Accès correct aux éléments
+    anonKey: passData[1][0],
+  );
+
+  // final supabase = Supabase.instance.client;
+  // final data = await supabase
+  //     .from('utilisateur')
+  //     .select();
+  // for (var elem in data){
+  //   print(elem);
+  // }
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
