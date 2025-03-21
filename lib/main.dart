6a87a93af1b131592ad508f122f183ff99cb5006
chris-mathevet <chris.mathevet@etu.univ-orleans.fr>
2025-03-21 13:40:14 +0100

    fin

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'UI/restaurantDetaiL.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
-    home: await RestaurantDetailPage.create("node/9136326362"),
+    // home: await RestaurantDetailPage.create("node/9136326362"), // Freshkin
+    home: await RestaurantDetailPage.create("node/3422189698"), // Cha+
   ));
 }
